796685bdf1475e1bf67a98788cfa4b487144330d
hanifagitfit <farhatunansari@gmail.com>
2022-12-05 16:16:48 +0530

    news

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,10 +17,20 @@ const Projects = () => {
                         <p>E-COMMERCE PRODUCT PAGE | FRONTEND MENTOR CHALLENGE</p>
 
                         <p className='prime'>This page is build with plain html,css and javaScript<br /> with add-to-cart functionality.  Image-slidder &<br /> responsive design.</p>
-                       
+
                         <button className='btn'><a href="https://hanifagitfit.github.io/ecommerce-product-page/">See here</a></button>
                     </div>
                 </div>
+
+                <div className="detail">
+                    <img src={news} alt="news" />
+                    <div className="box">
+                        <p>DailyHunt | News App</p>
+                        <p className='prime'>DailyHunt is a news app build on React using<br /> api [News Api] It shows almost every <br /> type of news,such as Sports,Entertainment,<br />  technology with infinte scroll and Top loading bar .</p>
+                        <button className='btn'><a href="https://github.com/hanifagitfit/newsapp">See here</a></button>
+                    </div>
+                </div>
+
                 <div className="detail">
                     <img src={advice} alt="advice" />
                     <div className="box">
@@ -31,25 +41,16 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className="detail">
-                    <img src={news} alt="news" />
-                    <div className="box">
-                        <p>DailyHunt | News App</p>
-                        <p className='prime'>DailyHunt is a news app build on React using<br /> api [News Api] It shows almost every <br /> type of news,such as Sports,Entertainment,<br/>  technology with infinte scroll and Top loading bar .</p>
-                        <button className='btn'><a href="https://github.com/hanifagitfit/newsapp">See here</a></button>
-                    </div>
-                </div>
-               
-                <div className="detail">
                     <img src={textutils} alt="" />
                     <div className="box">
                         <p>TEXTUTILS | WORD COUNTER APP </p>
-                        <p className='prime'>This react app is not just count a word or character<br/> but also calculate the reading time.You can make<br/> the whole phrase uppercase,lowercase & reverse<br/> the phrase too. Light mode is by default you can<br/> enable dark mode for ease of your eyes.</p>
-                
+                        <p className='prime'>This react app is not just count a word or character<br /> but also calculate the reading time.You can make<br /> the whole phrase uppercase,lowercase & reverse<br /> the phrase too. Light mode is by default you can<br /> enable dark mode for ease of your eyes.</p>
+
                         <button className='btn'><a href="https://hanifagitfit.github.io/my-app/">See here</a></button>
 
                     </div>
                 </div>
-        
+
             </div>
         </section>
     )
